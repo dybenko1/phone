550f636657a6af82fa14476248492a46c5e992e9
Dybenko Guajardo <dybenko1@yahoo.com.mx>
2025-11-02 16:29:01 +0800

    Initializing database

---
+++ main/init_db.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	dbname   = "dyb_demp"
+	password = "postgres"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+
+	phoneNumbers := []string{
+		"1234567890",
+		"123 456 7891",
+		"(123) 456 7892",
+		"(123) 456-7893",
+		"123-456-7894",
+		"123-456-7890",
+		"1234567892",
+		"(123)456-7892",
+	}
+
+	sqlStatement := `
+	INSERT INTO raw_numbers (first_name)
+	VALUES ($1)`
+
+	for _, number := range phoneNumbers {
+		_, err = db.Exec(sqlStatement, number)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
